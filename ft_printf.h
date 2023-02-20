8742ce555bc472fe62dd089fbb204c31a23e3ef6
Tigran Khachatryan <tikhacha@c2r2s6.42yerevan.am>
2023-02-20 15:12:45 +0400

    last commit before eval

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tikhacha <tikhacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 21:19:13 by tikhacha          #+#    #+#             */
-/*   Updated: 2023/02/16 18:47:28 by tikhacha         ###   ########.fr       */
+/*   Updated: 2023/02/20 15:07:34 by tikhacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,6 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 
-typedef struct arg_list
-{
-	char			*content;
-	struct s_list	*next;
-}					args_list;
-
 int	ft_printf(const char *arg, ...);
 int	ft_percentage(char type, va_list arg);
 int	ft_putchr(char c);
@@ -30,6 +24,6 @@ int	ft_putstr(char *s, int t);
 int	ft_putnbr(int nb);
 int	ft_putnbrun(unsigned int nb);
 int	ft_putnbrhex(unsigned int nb, char c);
-int ft_putptrhex(char const *ptr);
+int	ft_putptrhex(char const *ptr);
 
 #endif
